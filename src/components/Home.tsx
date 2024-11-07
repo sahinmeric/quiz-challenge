71133b531a7d64c080822ed3a014adec8cd90a8d
Sahin Meric <sahin.meric@medisprout.com>
2024-11-07 00:05:44 -0500

    home title updated with new class

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div className="relative min-h-screen flex items-center justify-center">
-      <h1 className="large-title">QUIZZLER</h1>
+      <h1 className="large-title bebas">QUIZZLER</h1>
       <p className="subtitle" onClick={() => navigate("/quiz")}>
         Let’s start the quiz →
       </p>
