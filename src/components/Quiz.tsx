17f67f54deb1fd426d0891c7e276be4fae92aa31
Sahin Meric <sahin.meric@medisprout.com>
2024-11-06 21:47:56 -0500

    Simple quiz component created

---
+++ src/components/Quiz.tsx
@@ -0,0 +1,10 @@
+function Quiz() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 text-blue-700">
+      <h1 className="text-4xl font-bold">Quiz Screen</h1>
+      <div>Quiz questions here</div>
+    </div>
+  );
+}
+
+export default Quiz;
