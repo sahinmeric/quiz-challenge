a44579badfbc2e04fe7b192da98172d6629a04a9
Sahin Meric <sahin.meric@medisprout.com>
2024-11-07 00:42:52 -0500

    removed mock data, used hook to bring data

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,45 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import QuestionCard from "./QuestionCard";
+import useFetchQuizQuestions from "../hooks/useFetchQuizQuestions";
 
 const Quiz = () => {
+  const { questions, loading, error } = useFetchQuizQuestions();
   const navigate = useNavigate();
-  const questions = [
-    {
-      question:
-        "Which of the following is the term for surgical complications resulting from surgical sponges left inside the patient's body?",
-      options: [
-        "Gauze grievance disorder",
-        "Retained surgical sponge syndrome",
-        "Sponge-induced septicemia",
-        "Fabric foreign object syndrome",
-      ],
-      correctAnswer: 1,
-    },
-    {
-      question:
-        "Which of the following is the term for surgical complications resulting from surgical sponges left inside the patient's body?",
-      options: [
-        "Gauze grievance disorder",
-        "Retained surgical sponge syndrome",
-        "Sponge-induced septicemia",
-        "Fabric foreign object syndrome",
-      ],
-      correctAnswer: 1,
-    },
-    {
-      question:
-        "Which of the following is the term for surgical complications resulting from surgical sponges left inside the patient's body?",
-      options: [
-        "Gauze grievance disorder",
-        "Retained surgical sponge syndrome",
-        "Sponge-induced septicemia",
-        "Fabric foreign object syndrome",
-      ],
-      correctAnswer: 1,
-    },
-  ];
-
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState<number[]>([]);
   const [score, setScore] = useState(0);
@@ -49,7 +15,12 @@ const Quiz = () => {
     updatedSelectedOptions[currentQuestionIndex] = index;
     setSelectedOptions(updatedSelectedOptions);
 
-    if (index === questions[currentQuestionIndex].correctAnswer) {
+    const currentQuestion = questions[currentQuestionIndex];
+    const correctAnswerIndex = Object.values(
+      currentQuestion.correct_answers
+    ).findIndex((value) => value === "true");
+
+    if (index === correctAnswerIndex) {
       setScore((prevScore) => prevScore + 1);
     }
   };
@@ -62,41 +33,50 @@ const Quiz = () => {
     navigate("/result", { state: { score, total: questions.length } });
   };
 
+  if (loading) return <p>Loading questions...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 text-blue-700 p-4">
-      <h2 className="text-2xl font-semibold mb-4">
-        Question {currentQuestionIndex + 1}/{questions.length}
-      </h2>
-      <QuestionCard
-        question={questions[currentQuestionIndex].question}
-        options={questions[currentQuestionIndex].options}
-        selectedOption={selectedOptions[currentQuestionIndex]}
-        onSelectOption={handleOptionSelect}
-      />
-      {currentQuestionIndex < questions.length - 1 ? (
-        <button
-          onClick={handleNextQuestion}
-          className={`mt-6 px-6 py-2 font-semibold rounded ${
-            selectedOptions[currentQuestionIndex] !== undefined
-              ? "bg-blue-700 text-white"
-              : "bg-gray-400 text-gray-200 cursor-not-allowed"
-          }`}
-          disabled={selectedOptions[currentQuestionIndex] === undefined}
-        >
-          Next
-        </button>
-      ) : (
-        <button
-          onClick={handleFinishQuiz}
-          className={`mt-6 px-6 py-2 font-semibold rounded ${
-            selectedOptions[currentQuestionIndex] !== undefined
-              ? "bg-blue-700 text-white"
-              : "bg-gray-400 text-gray-200 cursor-not-allowed"
-          }`}
-          disabled={selectedOptions[currentQuestionIndex] === undefined}
-        >
-          Finish
-        </button>
+      {questions.length > 0 && (
+        <>
+          <h2 className="text-6xl mb-4 bebas">
+            Question {currentQuestionIndex + 1}/{questions.length}
+          </h2>
+          <QuestionCard
+            question={questions[currentQuestionIndex]?.question}
+            options={Object.values(
+              questions[currentQuestionIndex]?.answers
+            ).filter((answer) => answer !== null)}
+            selectedOption={selectedOptions[currentQuestionIndex]}
+            onSelectOption={handleOptionSelect}
+          />
+          {currentQuestionIndex < questions.length - 1 ? (
+            <button
+              onClick={handleNextQuestion}
+              className={`mt-6 px-6 py-2 font-semibold rounded ${
+                selectedOptions[currentQuestionIndex] !== undefined
+                  ? "bg-blue-700 text-white"
+                  : "bg-gray-400 text-gray-200 cursor-not-allowed"
+              }`}
+              disabled={selectedOptions[currentQuestionIndex] === undefined}
+            >
+              Next
+            </button>
+          ) : (
+            <button
+              onClick={handleFinishQuiz}
+              className={`mt-6 px-6 py-2 font-semibold rounded ${
+                selectedOptions[currentQuestionIndex] !== undefined
+                  ? "bg-blue-700 text-white"
+                  : "bg-gray-400 text-gray-200 cursor-not-allowed"
+              }`}
+              disabled={selectedOptions[currentQuestionIndex] === undefined}
+            >
+              Finish
+            </button>
+          )}
+        </>
       )}
     </div>
   );
