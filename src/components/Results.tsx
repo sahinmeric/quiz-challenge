595ba6878e80e9f6d9e69457de3c153ca41a5356
Sahin Meric <sahin.meric@medisprout.com>
2024-11-07 00:17:54 -0500

    updated result screen UI

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -3,25 +3,24 @@ import { useLocation, useNavigate } from "react-router-dom";
 const Results = () => {
   const location = useLocation();
   const navigate = useNavigate();
-
   const { score, total } = location.state || { score: 0, total: 0 };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-700 text-white">
-      <div className="flex flex-col md:flex-row items-center text-center md:text-left">
-        <div className="md:mr-8">
-          <h1 className="text-6xl font-bold">BRAVO!</h1>
-          <p className="text-2xl mt-4">YOU HAVE SCORED</p>
-          <button
-            onClick={() => navigate("/quiz")}
-            className="mt-4 text-lg underline"
-          >
-            Wanna Play Again?
-          </button>
+    <div className="flex min-h-screen">
+      <div className="flex-1 flex flex-col items-center justify-center bg-blue-700 text-white p-8">
+        <h1 className="text-[180px] font-bold leading-[0.76]">BRAVO!</h1>
+        <p className="text-[80px] font-bold mt-4">YOU HAVE SCORED</p>
+        <div
+          className="mt-4 text-lg underline hover:text-gray-300 cursor-pointer"
+          onClick={() => navigate("/quiz")}
+        >
+          Wanna Play Again?
         </div>
-        <div className="mt-8 md:mt-0 md:ml-8 text-8xl font-bold">
+      </div>
+      <div className="flex-1 flex items-center justify-center bg-[#EBEFF2] text-blue-700">
+        <span className="text-[180px] font-bold leading-[0.76]">
           {score}/{total}
-        </div>
+        </span>
       </div>
     </div>
   );
